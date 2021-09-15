7f7dd4e359a2082a583e98ee8ce2cfe3f834c026
faid123 <faid_akatsuki@yahoo.com>
2021-09-15 06:51:11 -0700

    test

---
+++ courseworkdouble1/src/test/java/com/abc/a/courseworkdouble1/AppTest.java
@@ -0,0 +1,21 @@
+package com.abc.a.courseworkdouble1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("a");
+    }
+}
