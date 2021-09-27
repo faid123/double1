ec963fbf5a86243a32e4346fcfd3130c0f27cc65
faid123 <faid_akatsuki@yahoo.com>
2021-09-27 08:26:04 -0700

    implemented dice rolling

--- courseworkdouble1/src/test/java/com/abc/a/courseworkdouble1/dices.java
+++ courseworkdouble1/src/test/java/com/abc/a/courseworkdouble1/dices.java
@@ -1,12 +1,32 @@
 package com.abc.a.courseworkdouble1;
+import java.util.Random;
 
 public class dices {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("hello");
+		rollTwoDice();
+		System.out.println("Dice have been rolled successfully!");
 
+	}
+	
+	public static void rollTwoDice()
+	{
+		int min1 = 1;
+		int max1 = 6;
+		
+		int min2 = 1;
+		int max2 = 6;
+
+		Random random1 = new Random();
+		Random random2 = new Random();
+
+		int value1 = random1.nextInt(max1 + min1) + min1;
+		int value2 = random2.nextInt(max2 + min2) + min2;
+		
+		System.out.println("Dice 1: " + value1);
+		System.out.println("Dice 2: " + value2);
+		System.out.println("Total Rolled: " + (value1 + value2));
 	}
 
 }
